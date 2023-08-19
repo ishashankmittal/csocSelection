49092bcb9bcf19969320a9614256a01cf8b5fd4c
Shashank Mittal <117439240+ishashankmittal@users.noreply.github.com>
2023-08-19 19:16:52 +0530

    added sign in and sign up pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,3 +3,13 @@ module.exports.home = function(req, res){
         title: "Home"
     });
 }
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Sign In"
+    });
+}
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Sign Up"
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ console.log('router loaded');
 
 
 router.get('/', homeController.home);
-
+router.get('/user_sign_in', homeController.signIn);
+router.get('/user_sign_up', homeController.signUp);
 module.exports=router;
